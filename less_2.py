ad78af5752a6ecbe4c901956fee44a650bdcd217
Olga I <olgachao@mail.ru>
2023-01-04 13:02:09 +0600

    initial commoit less_2 file

---
+++ less_2.py
@@ -0,0 +1,69 @@
+# 1. Напишите программу, которая принимает на вход
+# вещественное число и показывает сумму его цифр.
+# Без работы с методами строк.
+# in -> out
+# - 6782 -> 23
+# - 0.67 -> 13
+# - 198.45 -> 27
+
+# def sum_digs_real_numb()
+# num = float(input('Enter your number, please: '))
+# num_round = int(input('Enter your number of decimal points, please:  '))
+# print(num, num_round)
+# num1 = num % 10
+# num2 = num / 10
+# num3 = num // 10
+# # num4 = num // 100* num
+# # while 10 * num // 10 > 0:
+# #     num = num / 10          # in 16.75 out 0.1675
+# # while num % 10 > 0:
+# #     num *= 10
+# # print(num/10, num // 10)
+
+# sum_dig = 0
+# while num // 10 > 0:
+  
+#     sum_dig = sum_dig + (num / 10) % 10
+# #     # print(num1, num2, num3)
+    
+# print('num = ',num, 'num1 = ', num1, 'sum = ', sum)
+
+
+# def flo_Sum ():
+# num = float(input('Enter your number, please: '))
+# while  num % 10 > 0:
+#     num = num * 10
+# print(int(num))
+
+# a = (int(num))
+# d = list(a)
+# for i in len:
+#     el_sum= sum(len)
+#     print(el_sum)
+    
+# flo_Sum(int())
+# num_new = list(flo_Sum)
+# print(num_new)
+
+# num = float(input('Enter your number, please: '))
+# while  num % 10 > 0:
+#         num = num * 10
+# return int(num)
+# sum_dig = list(int(num))
+# print(sum_dig)
+# for i in range (0, len):
+#     sum_dig = num[0] + num[len]
+# print (sum_dig)
+# sum 
+
+
+# def flo_Sum ():
+
+num = float(input('Enter your number, please: '))
+while  num % 10 > 0:
+    num = num * 10
+print(int(num))
+
+# def flo_Sum (a):
+#     a= (int(num))
+# print(flo_Sum)
